5d3f0110bf0756045bc000b986d620c39a338a61
KarlFarrar <89283702+KarlFarrar@users.noreply.github.com>
2025-02-05 11:09:11 -0600

    Small updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create!([
+    {title: "The Lord of the rings"},
+    {title: "Lord of the flies"},
+    {title: "Animal Farm"},
+    {title: "Emma"}
+])
